7c29901eac3ed92a4c5a393483afa2aadf0dede3
miriamfrank23 <miriamfrank23@gmail.com>
2018-10-16 16:44:25 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,8 @@
-//this file is optional
-//you can use this to paste in js code and have it run when index.html is loaded
+const element = document.createElement('div')
+element.innerHTML = 'Hello, DOM!'
+element.style.backgroundColor = 'pink'
+document.body.appendChild(element)
+element.style.textAlign = 'center'
+
+
+element.remove();
